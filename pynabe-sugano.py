960e5572bd6df902df570e5b580c12d56ddd97ff
ricardo-ayres <ricardo.bosqueiro.ayres@gmail.com>
2015-05-26 12:23:51 -0300

    about() fixed

--- pynabe-sugano.py
+++ pynabe-sugano.py
@@ -8,7 +8,7 @@ def help():
     print("Write this good!")
     
 def about():
-    print("Written by Ricardo B. Ayres in 2015.")
+    print("Python script that automates calculations involving Tanabe-Sugano diagrams \nThe task of finding the ratio of the heights of two lines with a ruler and then determining the x and y intercepts in Tanabe-Sugano diagrams is tedious and slow. This script utilizes 'diagrams' in the form of tables and searches for the ratio automatically and then calculates the B Racah parameter and 10Dq.")
     
 def get_diagram(d_electrons):
     path='resources/diagrams/'
