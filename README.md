34cff199559203cb8d2445d58ee07cdd5d1a5b06
ricardo-ayres <ricardo.bosqueiro.ayres@gmail.com>
2015-06-02 00:01:15 -0300

    applied the GNU GPLv3 license.

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
 # pynabe-sugano
-Python script that automates calculations involving Tanabe-Sugano diagrams
+
+###############################################################################
+#
+#   Pynabe-sugano is a calculator for Tanabe-Sugano diagrams.
+#   Copyright 2015 Ricardo B. Ayres
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+Pynabe-sugano is a python script that automates calculations involving Tanabe-Sugano diagrams
 
   The task of finding the ratio of the heights of two lines with a ruler and then determining the x and y intercepts in Tanabe-Sugano diagrams is tedious and slow. This script utilizes 'diagrams' in the form of tables and searches for the ratio automatically and then calculates the B Racah parameter and 10Dq.
+
--- pynabe-sugano.py
+++ pynabe-sugano.py
@@ -1,3 +1,23 @@
+###############################################################################
+#
+#   Pynabe-sugano is a calculator for Tanabe-Sugano diagrams.
+#   Copyright 2015 Ricardo B. Ayres
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
 #!/usr/bin/python2
 
 import sys
@@ -25,7 +45,16 @@ Options:
     sys.exit(0)
     
 def about():
-    print("Python script that automates calculations involving Tanabe-Sugano diagrams \nThe task of finding the ratio of the heights of two lines with a ruler and then determining the x and y intercepts in Tanabe-Sugano diagrams is tedious and slow. This script utilizes 'diagrams' in the form of tables and searches for the ratio automatically and then calculates the B Racah parameter and 10Dq.")
+    print("""
+    Pynabe-sugano is a calculator for Tanabe-Sugano diagrams.    
+    Pynabe-sugano  Copyright (C) 2015  Ricardo B. Ayres
+    
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+""")
     sys.exit(0)
     
 def get_diagram(d_electrons):
